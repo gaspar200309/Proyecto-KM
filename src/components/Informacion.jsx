661ebeea8c30cd03a56accedb2953d5337ab6718
Nahuel <nahueltorrezg@gmail.com>
2024-09-13 12:41:24 -0400

    Update section information

--- src/components/Informacion.jsx
+++ src/components/Informacion.jsx
@@ -1,15 +1,17 @@
+import Logo from './../assets/logos/LogoKM.png'
+
 export const Informacion = () => {
 	return (
 		<>
 			<section className="nosotros contenedor" id="nosotros">
-				<h2>PÁGINA PROFESIOGRÁFICA</h2>
+				<img src={Logo} alt="Kawsay Muju" />
+
 				<div>
-					<div className="texto-nosotros">
-						<p>
-							Hola bienvenid@ a la página Profesiográfica Semillas. En este sitio encontrarás información sobre diferentes centros de
-							educación superior y carreras que podrías estudiar.{' '}
-						</p>
-					</div>
+					<h2>ORIENTACIÓN VOCACIONAL</h2>
+					<p className="texto-nosotros">
+						Hola bienvenid@ a la página Profesiográfica Semillas. En este sitio encontrarás información sobre diferentes centros de
+						educación superior y carreras que podrías estudiar.
+					</p>
 				</div>
 			</section>
 		</>
