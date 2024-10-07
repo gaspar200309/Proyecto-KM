419f80e59e2d46f9e03f55b79358b460c916a8ca
gaspar200309 <133708167+gaspar200309@users.noreply.github.com>
2024-10-07 06:17:09 -0400

    deploy

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,103 +1,119 @@
-import { NavLink, Link } from 'react-router-dom'
-import { Button } from './Buttons/button.jsx'
-import './Nav2.css'
+import { NavLink, Link, useLocation } from 'react-router-dom';
+import { Button } from './Buttons/button.jsx';
+import './Nav2.css';
 import Buscador from './Buscador.jsx';
+import { useState } from 'react';
 
 const NavBarReact = () => {
-	return (
-		<header>
-			<div className="container2">
-				<input type="checkbox" name="" id="check" />
-				<div className="nav-btn">
-					<div className="nav-links">
-						<ul>
-							<li className="nav-link" style={{ '--i': '.6s' }}>
-								<NavLink to="/">Inicio</NavLink>
-							</li>
-							<li className="nav-link" style={{ '--i': '.85s' }}>
-								<NavLink to="carrera">
-									Carreras<i className="fas fa-caret-down"></i>
-								</NavLink>
-								<div className="dropdown">
-									<ul>
-										<li className="dropdown-link">
-											<NavLink to="#">Área salud</NavLink>
-										</li>
-										<li className="dropdown-link">
-											<NavLink to="#">Ingenierías</NavLink>
-										</li>
-										<li className="dropdown-link">
-											<NavLink to="#">Carreras empresariales</NavLink>
-										</li>
-										<li className="dropdown-link">
-											<NavLink to="#">Carreras Tecnológicas</NavLink>
-										</li>
-										<li className="dropdown-link">
-											<NavLink to="#">Carreras sociales</NavLink>
-										</li>
-										<li className="dropdown-link">
-											<Link to="#territorio">Urbanismo y territorio</Link>
-										</li>
-										<div className="arrow"></div>
-									</ul>
-									<div className="arrow"></div>
-								</div>
-							</li>
-							<li className="nav-link" style={{ '--i': '1.1s' }}>
-								<NavLink to="facultad">
-									Universidades<i className="fas fa-caret-down"></i>
-								</NavLink>
-								<div className="dropdown">
-									<ul>
-										<li className="dropdown-link">
-											<NavLink to="#">universidades publicas</NavLink>
-										</li>
-										<li className="dropdown-link">
-											<NavLink to="#">Univeridades privadas</NavLink>
-										</li>
-										<li className="dropdown-link">
-											<NavLink to="#">Normales</NavLink>
-										</li>
-										<li className="dropdown-link">
-											<NavLink to="#">Institutos publicas</NavLink>
-										</li>
-										<li className="dropdown-link">
-											<NavLink to="#">Institutos privadas</NavLink>
-										</li>
-										<div className="arrow"></div>
-									</ul>
-								</div>
-							</li>
-							<li className="nav-link" style={{ '--i': '1.35s' }}>
-								<NavLink to="becas">Becas</NavLink>
-							</li>
-							<li className="nav-link" style={{ '--i': '1.35s' }}>
-								<Link to="/instituto">Institutos</Link>
-							</li>
-						</ul>
-					</div>
-					<div>
-						<ul className ="navBar-search">
-            <li>
-                <Buscador></Buscador>
+  const location = useLocation();
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const shouldShowBuscador = location.pathname === '/';
+
+  // Función para cerrar el menú
+  const closeMenu = () => setMenuOpen(false);
+
+  return (
+    <header>
+      <div className="container2">
+        <input 
+          type="checkbox" 
+          id="check" 
+          checked={isMenuOpen} 
+          onChange={() => setMenuOpen(!isMenuOpen)} // Toggle del menú
+        />
+        <div className="nav-btn">
+          <div className="nav-links">
+            <ul>
+              <li className="nav-link" style={{ '--i': '.6s' }}>
+                <NavLink to="/" onClick={closeMenu}>Inicio</NavLink>
               </li>
-							<li className="nav-link" style={{ '--i': '1.35s' }}>
-								<NavLink to="login">
-									<Button texto="Ingresar" />
-								</NavLink>
-							</li>
-						</ul>
-					</div>
-				</div>
+              <li className="nav-link" style={{ '--i': '.85s' }}>
+                <NavLink to="/carrera" onClick={closeMenu}>
+                  Carreras<i className="fas fa-caret-down"></i>
+                </NavLink>
+                <div className="dropdown">
+                  <ul>
+                    <li className="dropdown-link">
+                      <NavLink to="#" onClick={closeMenu}>Área salud</NavLink>
+                    </li>
+                    <li className="dropdown-link">
+                      <NavLink to="#" onClick={closeMenu}>Ingenierías</NavLink>
+                    </li>
+                    <li className="dropdown-link">
+                      <NavLink to="#" onClick={closeMenu}>Carreras empresariales</NavLink>
+                    </li>
+                    <li className="dropdown-link">
+                      <NavLink to="#" onClick={closeMenu}>Carreras Tecnológicas</NavLink>
+                    </li>
+                    <li className="dropdown-link">
+                      <NavLink to="#" onClick={closeMenu}>Carreras sociales</NavLink>
+                    </li>
+                    <li className="dropdown-link">
+                      <Link to="#territorio" onClick={closeMenu}>Urbanismo y territorio</Link>
+                    </li>
+                    <div className="arrow"></div>
+                  </ul>
+                  <div className="arrow"></div>
+                </div>
+              </li>
+              <li className="nav-link" style={{ '--i': '1.1s' }}>
+                <NavLink to="/facultad" onClick={closeMenu}>
+                  Universidades<i className="fas fa-caret-down"></i>
+                </NavLink>
+                <div className="dropdown">
+                  <ul>
+                    <li className="dropdown-link">
+                      <NavLink to="#" onClick={closeMenu}>Universidades públicas</NavLink>
+                    </li>
+                    <li className="dropdown-link">
+                      <NavLink to="#" onClick={closeMenu}>Universidades privadas</NavLink>
+                    </li>
+                    <li className="dropdown-link">
+                      <NavLink to="#" onClick={closeMenu}>Normales</NavLink>
+                    </li>
+                    <li className="dropdown-link">
+                      <NavLink to="#" onClick={closeMenu}>Institutos públicos</NavLink>
+                    </li>
+                    <li className="dropdown-link">
+                      <NavLink to="#" onClick={closeMenu}>Institutos privados</NavLink>
+                    </li>
+                    <div className="arrow"></div>
+                  </ul>
+                </div>
+              </li>
+              <li className="nav-link" style={{ '--i': '1.35s' }}>
+                <NavLink to="/becas" onClick={closeMenu}>Becas</NavLink>
+              </li>
+              <li className="nav-link" style={{ '--i': '1.35s' }}>
+                <Link to="/instituto" onClick={closeMenu}>Institutos</Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <ul className="navBar-search">
+              {shouldShowBuscador && (
+                <li>
+                  <Buscador />
+                </li>
+              )}
+              <li className="nav-link" style={{ '--i': '1.35s' }}>
+                <NavLink to="/login" onClick={closeMenu}>
+                  <Button texto="Ingresar" />
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
 
-				<div className="hamburger-menu-container">
-					<div className="hamburger-menu">
-						<div></div>
-					</div>
-				</div>
-			</div>
-		</header>
-	)
-}
+        <div className="hamburger-menu-container">
+          <div className="hamburger-menu" onClick={() => setMenuOpen(!isMenuOpen)}>
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default NavBarReact
+export default NavBarReact;
