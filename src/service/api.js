d91d6a6e154a524bba7e576526308b7bc4bba920
gaspar200309 <133708167+gaspar200309@users.noreply.github.com>
2024-10-15 05:48:15 -0400

    fix: update direction backend

--- src/service/api.js
+++ src/service/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const baseURL =  "http://localhost:3000";
-/* const baseURL = process.env.NODE_ENV === 'production'
+/* const baseURL =  "http://localhost:3000"; */
+const baseURL = process.env.NODE_ENV === 'production'
   ? "https://backend-km-git-main-gaspar200309s-projects.vercel.app"
-  : "http://localhost:3000"; */
+  : "https://backend-km-git-main-gaspar200309s-projects.vercel.app";
 
 
 const api = axios.create({
